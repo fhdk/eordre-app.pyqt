a911ccd7e93e6f98eead290870a5be92ce40e590
@fhdk <fh@uex.dk>
2018-02-08 12:32:38 +0100

    refactor calls

--- models/visit.py
+++ models/visit.py
@@ -51,6 +51,13 @@ class Visit:
             self.q.execute(sql)
 
     @property
+    def csv_record_length(self):
+        """
+        The number of fields expected on csv import
+        """
+        return self._csv_record_length
+
+    @property
     def visit(self):
         """
         Visit
@@ -59,55 +66,8 @@ class Visit:
         """
         return self._visit
 
-    @visit.setter
-    def visit(self, visit_id):
-        """
-        Set a visit
-        :param visit_id:
-        :return:
-        """
-        self.__get(visit_id)
-
     @property
-    def csv_record_length(self):
-        """
-        The number of fields expected on csv import
-        """
-        return self._csv_record_length
-
-    @property
-    def list_by_customer(self):
-        """
-        The list of visits for a customer
-        Returns:
-            The list of visits for a customer
-        """
-        return self._visits
-
-    @list_by_customer.setter
-    def list_by_customer(self, customer_id):
-        """
-        Load the list of visits for a customer_id
-        Args:
-            customer_id:
-        """
-        self.__get_by_customer(customer_id)
-
-    @property
-    def list_by_date(self):
-        return self._visits
-
-    @list_by_date.setter
-    def list_by_date(self, workdate):
-        """
-        Load the list of visits for a given date
-        :param workdate:
-        :return:
-        """
-        self.__get_by_date(workdate)
-
-    @property
-    def list_by_report_id(self):
+    def visits(self):
         """
         Report Visit List
         Returns:
@@ -115,21 +75,37 @@ class Visit:
         """
         return self._visits
 
-    @list_by_report_id.setter
+    def list_by_customer(self, customer_id):
+        """
+        Load the list of visits for a given customer
+        Args:
+            customer_id:
+        """
+        self.__get_by_customer(customer_id)
+
+    def list_by_date(self, visit_date):
+        """
+        Load the list of visits for a given date
+        Args:
+             visit_date:
+        """
+        self.__get_by_date(visit_date)
+
     def list_by_report_id(self, report_id):
         """
-        Load visits for the requested report_id
+        Load the list of visits for a given report
         Args:
             report_id:
         """
         self.__get_by_report_id(report_id)
 
-    def clear(self):
+    def load_visit(self, visit_id):
         """
-        Clear internal variables
+        Load a visit
+        Args:
+            visit_id:
         """
-        self._visit = {}
-        self._visits = []
+        self.__get(visit_id)
 
     def add(self, report_id, employee_id, customer_id, workdate):
         """
@@ -148,6 +124,13 @@ class Visit:
         self.__get(new_id)
         return new_id
 
+    def clear(self):
+        """
+        Clear internal variables
+        """
+        self._visit = {}
+        self._visits = []
+
     def delete(self, visit_id):
         """
         Delete the specified visit
@@ -158,6 +141,58 @@ class Visit:
         values = (visit_id,)
         sql = self.q.build("delete", self.model, filters)
         self.q.execute(sql, values)
+
+    def insert(self, values):
+        """
+        Insert a new row in the database
+        :param values:
+        :return: rownumber on success or None
+        """
+        sql = self.q.build("insert", self.model)
+        success, data = self.q.execute(sql, values=values)
+        if success and data:
+            return data
+        return None
+
+    def recreate_table(self):
+        """
+        Recreate table
+        """
+        sql = self.q.build("drop", self.model)
+        self.q.execute(sql)
+        sql = self.q.build("create", self.model)
+        self.q.execute(sql)
+        self.clear()
+
+    def translate_row_insert(self, row):
+        """
+        Translate a csv row
+        :rtype: bool
+        :param row:
+        :return: True / False
+        """
+        # translate bool text to integer col 5
+        field_5 = utils.bool2int(utils.arg2bool(row[5]))
+        new_row = (row[0], row[1], row[2], row[3], row[4].strip(),
+                   field_5, row[6].strip(), row[7].strip(), row[8].strip(), row[9].strip(),
+                   row[10].strip(), row[11].strip(), row[12].strip(), row[13].strip(), row[14].strip(),
+                   row[15].strip(), row[16].strip(), row[17].strip(), row[18], row[19],
+                   row[20], row[21], row[14].strip())
+        self.insert(new_row)  # call insert function
+
+    def update(self):
+        """
+        Write visit changes to database
+        :return: rownumber on success or None
+        """
+        fields = list(self.model["fields"])[1:]
+        filters = [(self.model["id"], "=")]
+        values = self.q.values_to_update(self._visit.values())
+        sql = self.q.build("update", self.model, update=fields, filters=filters)
+        success, data = self.q.execute(sql, values=values)
+        if success and data:
+            return data
+        return None
 
     def __get(self, visit_id):
         """
@@ -234,55 +269,3 @@ class Visit:
             except (IndexError, KeyError):
                 self._visit = {}
                 self._visits = []
-
-    def insert(self, values):
-        """
-        Insert a new row in the database
-        :param values:
-        :return: rownumber on success or None
-        """
-        sql = self.q.build("insert", self.model)
-        success, data = self.q.execute(sql, values=values)
-        if success and data:
-            return data
-        return None
-
-    def translate_row_insert(self, row):
-        """
-        Translate a csv row
-        :rtype: bool
-        :param row:
-        :return: True / False
-        """
-        # translate bool text to integer col 5
-        field_5 = utils.bool2int(utils.arg2bool(row[5]))
-        new_row = (row[0], row[1], row[2], row[3], row[4].strip(),
-                   field_5, row[6].strip(), row[7].strip(), row[8].strip(), row[9].strip(),
-                   row[10].strip(), row[11].strip(), row[12].strip(), row[13].strip(), row[14].strip(),
-                   row[15].strip(), row[16].strip(), row[17].strip(), row[18], row[19],
-                   row[20], row[21], row[14].strip())
-        self.insert(new_row)  # call insert function
-
-    def recreate_table(self):
-        """
-        Recreate table
-        """
-        sql = self.q.build("drop", self.model)
-        self.q.execute(sql)
-        sql = self.q.build("create", self.model)
-        self.q.execute(sql)
-        self.clear()
-
-    def update(self):
-        """
-        Write visit changes to database
-        :return: rownumber on success or None
-        """
-        fields = list(self.model["fields"])[1:]
-        filters = [(self.model["id"], "=")]
-        values = self.q.values_to_update(self._visit.values())
-        sql = self.q.build("update", self.model, update=fields, filters=filters)
-        success, data = self.q.execute(sql, values=values)
-        if success and data:
-            return data
-        return None
